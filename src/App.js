d7b4d8ec885e287feeb526a7a4818726a6a4570c
Plinio Grijalba <plinio.grijalba@davincitech.co>
2020-02-01 13:39:03 -0500

    phonebook init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: 465464 }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ filterInput, setFilterInput ] = useState('ccccccccc')
+
+  const personsToShow = persons.filter(person => person.name.indexOf(filterInput) > -1)
+
+  const findName = () => {
+    return persons.find(a => a.name === newName)
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+    if(findName()){
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
+    const newPersons = persons.concat([{name: newName, number: newNumber}]);
+    setPersons(newPersons);
+    setNewName('');
+    setNewNumber('');
+  }
+  
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setFilterInput(event.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        flter shown with: <input
+          value={filterInput}
+          onChange={handleFilterChange}
+        />
+      </div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input
+            value={newName}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          number: <input
+            value={newNumber}
+            onChange={handleNumberChange}
+          />
+        </div>
+        <div>
+          <button type="submit" >add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {personsToShow.map(p => <li>{p.name} {p.number}</li>)}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
